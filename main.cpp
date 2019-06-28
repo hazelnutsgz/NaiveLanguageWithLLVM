8b9df64fe803177179b621caabb0331746c8f3b2
hazelnutsgz <15201752137@163.com>
2019-06-28 01:43:22 -0500

    Finish parsing

--- main.cpp
+++ main.cpp
@@ -13,11 +13,10 @@ enum Token {
 
     // commands
     TokenDef = -2,
-    TokenExtern = -3,
 
     // primary
-    TokenIdentifier = -4,
-    TokenNumber = -5
+    TokenIdentifier = -3,
+    TokenNumber = -4
 };
 
 std::string identifierStr; // Filled in if tok_identifier
@@ -32,7 +31,6 @@ int GetToken() {
         while (isalnum((LastChar = GetChar()))) identifierStr += lastChar;
 
         if (identifierStr == "def") return TokenDef;
-        if (identifierStr == "extern") return TokenExtern;
     
         return TokenIdentifier;  
     }  
@@ -65,15 +63,53 @@ int GetNextToken() {
 std::map<char, int> BinopPrecedence;
 
 int GetTokenPrecedence() {
-  if (!isascii(CurrentToken)) return -1;
+    if (!isascii(CurrentToken)) return -1;
 
-  // Make sure it's a declared binop.
-  int tokenPrecedence = BinopPrecedence[CurrentToken];
-  if (tokenPrecedence <= 0) return -1;
-  return tokenPrecedence;
+    // Make sure it's a declared binop.
+    int tokenPrecedence = BinopPrecedence[CurrentToken];
+    if (tokenPrecedence <= 0) return -1;
+    return tokenPrecedence;
 }
 
-std::unique_ptr<ExprAST> ParseExpression();
+std::unique_ptr<ExprAST> ParseBinOpRHS(int expressionPrecedence,
+                        std::unique_ptr<ExprAST> LHS) {
+    // If this is a binop, find its precedence.
+    while (true) {
+        int tokenPrecedence = GetTokenPrecedence();
+
+        // If this is a binop that binds at least as tightly as the current binop,
+        // consume it, otherwise we are done.
+        if (tokenPrecedence < expressionPrecedence) return LHS;
+
+        // Okay, we know this is a binop.
+        int binaryOperator = CurrentToken;
+        GetNextToken(); // eat binop
+
+        // Parse the primary expression after the binary operator.
+        auto RHS = ParsePrimary();
+        if (!RHS) return nullptr;
+
+        // If BinOp binds less tightly with RHS than the operator after RHS, let
+        // the pending operator take RHS as its LHS.
+        int nextPrecedence = GetTokenPrecedence();
+        if (tokenPrecedence < nextPrecedence) {
+            RHS = ParseBinOpRHS(tokenPrecedence + 1, std::move(RHS));
+            if (!RHS) return nullptr;
+        }
+
+        // Merge LHS/RHS.
+        LHS = llvm::make_unique<BinaryExprAST>(binaryOperator, 
+                                                std::move(LHS),
+                                                std::move(RHS));
+    }
+}
+
+std::unique_ptr<ExprAST> ParseExpression() {
+    auto LHS = ParsePrimary();
+    if (!LHS) return nullptr;
+
+    return ParseBinOpRHS(0, std::move(LHS));
+}
 
 std::unique_ptr<ExprAST> ParseNumberExpr() {
     auto Result = llvm::make_unique<NumberExprAST>(numVal);
@@ -160,6 +196,45 @@ void HandleTopLevelExpression() {
     }
 }
 
+std::unique_ptr<PrototypeAST> ParsePrototype() {
+    if (CurrentToken != tokenIdentifier) return LogErrorP("Expected function name in prototype");
+
+    std::string functionName = identifierStr;
+    getNextToken();
+
+    if (CurrentToken != '(') return LogErrorP("Expected '(' in prototype");
+
+    std::vector<std::string> argNames;
+    while (getNextToken() == tokenIdentifier) 
+        argNames.push_back(identifierStr);
+    if (CurrentToken != ')')
+        return LogErrorP("Expected ')' in prototype");
+
+    // success.
+    getNextToken(); // eat ')'.
+
+    return llvm::make_unique<PrototypeAST>(FnName, std::move(ArgNames));
+}
+
+std::unique_ptr<FunctionAST> ParseDefinition() {
+    getNextToken(); // eat def.
+    auto Proto = ParsePrototype();
+    if (!Proto) return nullptr;
+
+    if (auto E = ParseExpression())
+        return llvm::make_unique<FunctionAST>(std::move(Proto), std::move(E));
+    return nullptr;
+}
+
+void HandleDefinition() {
+    if (ParseDefinition()) {
+        fprintf(stderr, "Parsed a function definition.\n");
+    } else {
+        // Skip token for error recovery.
+        getNextToken();
+    }
+}
+
 void MainLoop() {
     while (true) {
         fprintf(stderr, "ready> ");
@@ -171,9 +246,6 @@ void MainLoop() {
                 break;
             case TokenDef:
                 HandleDefinition();
-                break;
-            case TokenExtern:
-                HandleExtern();
                 break;
             default:
                 HandleTopLevelExpression();
